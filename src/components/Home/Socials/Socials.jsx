e4b796f1aa742cecd85d4f9849c15ddde64b0b84
Mayank Kumar Gupta <66107976+mkgupta01@users.noreply.github.com>
2023-05-30 11:24:17 +0530

    Update Socials.jsx
    Corrected GitHub Profile Link

--- src/components/Home/Socials/Socials.jsx
+++ src/components/Home/Socials/Socials.jsx
@@ -4,7 +4,7 @@ import {  FaGithub , FaTwitter , FaLinkedin ,FaInstagram, FaDiscord } from 'reac
 function Socials(){
     return(
         <div className="soicalicons-containner">
-            <a href="https://twitter.com/mkgupta01">< FaGithub  fade/></a>
+            <a href="https://github.com/mkgupta01">< FaGithub  fade/></a>
             <a href="https://twitter.com/imayankgupta001">< FaTwitter /></a>
             <a href="https://www.linkedin.com/in/mkgupta001/">< FaLinkedin /></a>
             <a href="https://www.instagram.com/i_mayankgupta001/">< FaInstagram /></a>
