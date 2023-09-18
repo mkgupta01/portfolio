af6bf2bed0fb7c3019df65ae1498f85a0a0a977a
mkgupta01 <mk4693480@gmail.com>
2023-09-18 23:11:06 +0530

    removed loading screen

--- src/App.js
+++ src/App.js
@@ -2,19 +2,33 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  }, [])
+
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={ <Projects/> }/>
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      {loading === false ? (
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+          <Footer />
+        </Router>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
   )
 }
 
